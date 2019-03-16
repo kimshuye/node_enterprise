c318385d6f6de644264e6ad0dbaeddd4f04eaa01
Suntisuk Khemphed <suntisuk.k@piesoft.net>
2019-03-16 13:29:01 +0700

    Untag node_modules

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import MongoDB from './lib/mongodb'
 import CONFIG from '../config'
 
 const app = new express()
-const PORT = 3000
+const PORT = 3001
 
 app.get('/', (req, res) => { res.send('Hello World') })
 
 const db = new MongoDB('membership')
 db.open((connected) => console.log('is connected', connected))
-// console.log(CONFIG)
+console.log(CONFIG)
 
 app.listen(PORT)
 
